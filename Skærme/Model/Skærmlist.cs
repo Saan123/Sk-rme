8596443a187f3ab272747aebff6ebfd37c72a0b8
Saan Sivaguru <070895@live.dk>
2016-11-17 10:27:26 +0100

    Add project files.

---
+++ Skærme/Model/Skærmlist.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Skærme.Model
+{
+    public class Skærmlist : ObservableCollection<skærme>
+    {
+        
+        public Skærmlist()
+            :base()
+        {
+            this.Add(new skærme() { mærke = "iPhone", model = "5", farve = "sort", pris = "49kr.9" });
+            this.Add(new skærme() { mærke = "iPhone", model = "6", farve = "sort", pris = "749kr." });
+           
+        }
+        
+        //sample data
+
+
+        
+    }
+}
---
+++ Skærme/Model/skærme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skærme
+{       //Lager over mærke, modeller, farve, og pris
+    public class skærme
+    {
+        public string mærke {get; set;}
+        public string model { get; set; }
+        public string farve { get; set; }
+        public string pris { get; set; }
+    }
+}
